8e17246fcd36130e15b350175191760c380cf46c
Giovana Gama <giovana.gama.dc@gmail.com>
2026-03-04 22:12:04 -0300

    fix(app): removing unused files

--- teste.py
+++
@@ -1,8 +0,0 @@
-from tcgdexsdk import Query, TCGdex
-
-# Init the SDK
-tcgdex = TCGdex()
-# Or use the sync version
-card = tcgdex.card.listSync(Query().contains("name", 'clefairy'))
-
-print(f"Found: {card}")
